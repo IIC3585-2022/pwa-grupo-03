08ce4e9e79fd5e6a0a5013905dd12af61d8dd6be
agustin-rios <arios6@uc.cl>
2022-05-01 19:54:43 -0400

    feat(auth)
    : auth credentials

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,25 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyD25QX8FXrWCnAlhmBd-pCaSHnx50lVt1c",
-  authDomain: "pwa-js-46528.firebaseapp.com",
-  projectId: "pwa-js-46528",
-  storageBucket: "pwa-js-46528.appspot.com",
-  messagingSenderId: "131526367213",
-  appId: "1:131526367213:web:27c69b7f0e1b2084a479e4"
+const config = {
+  apiKey: "AIzaSyBLPBbjTe9JiL_-lLOqeJpcZC0rWJtkLBM",
+  authDomain: "pwa-js-d2b3f.firebaseapp.com",
+  projectId: "pwa-js-d2b3f",
+  storageBucket: "pwa-js-d2b3f.appspot.com",
+  messagingSenderId: "1036487722266",
+  appId: "1:1036487722266:web:bf5da649c8af625ed658a3"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
     throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
+      'Add your web app\'s configuration object to firebase-config.js');
   } else {
     return config;
   }
