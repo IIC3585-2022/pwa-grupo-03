c9eaacbb22e07dad69e9cd5d624d1eae3d5d4ab5
mfocqueteau <mfocqueteau@uc.cl>
2022-05-08 23:07:46 -0400

    perdi instalabilidad

--- public/app.js
+++ public/app.js
@@ -1,8 +1,31 @@
-
 /* Regisration */
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('firebase-messaging-sw.js')
   navigator.serviceWorker.register('sw.js')
 }
 
-// TODO: Implementar notificaciones
+// setInterval(, 1000)
+
+
+// let deferredPrompt;
+// const btnAdd = document.getElementById('btnAdd')
+// window.addEventListener('beforeinstallprompt', evt => {
+//   console.log('beforeinstall')
+//   evt.preventDefault()
+//   deferredPrompt = evt
+//   btnAdd.style.display = 'block'
+// })
+
+// btnAdd.addEventListener('click', () => {
+//   console.log('Ouch')
+//   deferredPrompt.prompt()
+//   deferredPrompt.userChoice.then(choice => {
+//     if (choice.outcome === 'accepted')
+//       console.log('Instalar!')
+//     deferredPrompt = null
+//   })
+// })
+
+// window.addEventListener('appinstalled', () => {
+//   app.logEvent('a2hs', 'installed')
+// })
--- public/sw.js
+++ public/sw.js
@@ -2,27 +2,28 @@
 
 // workbox.routing.registerRoute(
 //   ({ request }) => request.destination === 'image',
-//   // new workbox.strategies.CacheFirst()
-//   new workbox.strategies.NetworkFirst()
+//   new workbox.strategies.CacheFirst()
+//   // new workbox.strategies.NetworkFirst()
 // )
 
 /* Constants & Variables */
 
+
 const STATIC_CACHE = 's-pwa03-v1'
 const DYNAMIC_CACHE = 'd-pwa03-v1'
 const IMMUTABLE_CACHE = 'i-pwa03-v1'
+const ALL_CACHE = [STATIC_CACHE, IMMUTABLE_CACHE, DYNAMIC_CACHE]
 
 const MSG_LISTENER_ID = '/google.firestore.v1.Firestore/Listen/'
 
 const APP_SHELL = [
   '/',
-  './',
-  'app.js',
-  'index.html',
-  'firebase-messaging-sw.js',
-  'manifest.json',
-  'sw.js',
-  '../icons/manifest-icon-192.maskable.png',
+  '/app.js',
+  '/index.html',
+  '/firebase-messaging-sw.js',
+  '/manifest.json',
+  '/sw.js',
+  'icons/manifest-icon-192.maskable.png',
   'images/amogus.png',
   'scripts/main.js',
   'styles/main.css',
@@ -32,11 +33,11 @@ const EXTERNAL_RESOURCES = [
   "https://code.getmdl.io/1.1.3/material.min.js",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
   "https://code.getmdl.io/1.1.3/material.orange-indigo.min.css",
-  "https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en",
-  "https://apis.google.com/js/api.js?onload=__iframefcb317439",
+  "https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en",
   "https://fonts.gstatic.com/s/materialicons/v128/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
   "https://fonts.gstatic.com/s/roboto/v29/KFOlCnqEu92Fr1MmEU9fBBc4AMP6lQ.woff2",
   "https://fonts.gstatic.com/s/roboto/v29/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2",
+  "https://fonts.gstatic.com/s/roboto/v29/KFOkCnqEu92Fr1Mu51xIIzIXKMny.woff2"
 ]
 
 
@@ -45,9 +46,6 @@ const EXTERNAL_RESOURCES = [
 // async function requestBackgroundSync() {
 //   await self.registration.sync.register('msg-received');
 // }
-function identity(args) {
-  return args
-}
 
 
 /* Listeners */
@@ -65,20 +63,21 @@ self.addEventListener('install', evt => {
 })
 
 /* Activate */
-self.addEventListener('activate', (e) => {
-  console.log('Activate event was triggered!')
-  e.waitUntil(caches.keys().then((keyList) => {
+self.addEventListener('activate', evt => {
+  // Borrando cache obsoleto
+  evt.waitUntil(caches.keys().then((keyList) => {
     return Promise.all(keyList.map((key) => {
-      if ([STATIC_CACHE, IMMUTABLE_CACHE, DYNAMIC_CACHE].includes(key)) return
+      if (ALL_CACHE.includes(key)) return
       return caches.delete(key)
     }))
   }));
 });
 
 /* Manejo de fetch */
-self.addEventListener('fetch', async (evt) => {
+self.addEventListener('fetch', evt => {
   const response = caches.match(evt.request).then(res => {
-    if (res) return res
+    if (res) return res  // Estrategia cache-first
+
 
     // Si no existe el archivo en el caché, hacemos fetch en la red:
     return fetch(evt.request).then(newResp => {
@@ -88,17 +87,6 @@ self.addEventListener('fetch', async (evt) => {
           evt.request.url.includes(MSG_LISTENER_ID)  // Request cambia cada vez
         ))
           cache.put(evt.request, newResp)
-        else if (evt.request.url.includes(MSG_LISTENER_ID)) {
-          cache.match('msgHistory').then(msgHistResponse => {
-            const history = msgHistResponse || new Response('', { status: 200 })
-            history.text().then(histText => {
-              newResp.text().then(text => {
-                const finalResp = new Response(histText + text, { status: 200 })
-                cache.put('msgHistory', finalResp)
-              })
-            })
-          })
-        }
       })
       return newResp.clone()
     })
@@ -108,5 +96,5 @@ self.addEventListener('fetch', async (evt) => {
 })
 
 self.addEventListener('sync', evt => {
-  console.log('Hurraaa! Volvió la conexión!')
+  console.log('Hubo sync!')
 })
